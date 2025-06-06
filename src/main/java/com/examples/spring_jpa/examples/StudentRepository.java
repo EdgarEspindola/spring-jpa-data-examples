61b3e3919c1d9f689378b97b209c94dc11cedd7a
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2025-06-06 15:11:24 -0600

    Refactor StudentRepository to use named parameters in query methods for improved readability

--- src/main/java/com/examples/spring_jpa/examples/StudentRepository.java
+++ src/main/java/com/examples/spring_jpa/examples/StudentRepository.java
@@ -2,16 +2,17 @@ package com.examples.spring_jpa.examples;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
-
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    @Query("SELECT s FROM Student s WHERE s.email = ?1")
-    Optional<Student> findStudentByEmail(String email);
+    @Query("SELECT s FROM Student s WHERE s.email = :age")
+    Optional<Student> findStudentByEmail(@Param("age") String email);
 
-    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
-    List<Student> findStudentsByFirstNameEqualsIgnoreCaseAndAgeGreaterThanEqual(String firstName, Integer age); 
+    @Query("SELECT s FROM Student s WHERE s.firstName = :firstName AND s.age >= :age")
+    List<Student> findStudentsByFirstNameEqualsIgnoreCaseAndAgeGreaterThanEqual(@Param("firstName") String firstName,
+            @Param("age") Integer age);
 
 }
