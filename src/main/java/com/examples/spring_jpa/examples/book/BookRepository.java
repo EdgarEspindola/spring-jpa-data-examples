3f07fd74bce5e821c32377495c73931c8519cc1c
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2025-06-14 12:41:16 -0600

    fix(book): update BookResponse queries to remove createdAt field and adjust return type

--- src/main/java/com/examples/spring_jpa/examples/book/BookRepository.java
+++ src/main/java/com/examples/spring_jpa/examples/book/BookRepository.java
@@ -1,6 +1,7 @@
 package com.examples.spring_jpa.examples.book;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,12 +12,12 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     @Query("SELECT b FROM Book b JOIN FETCH b.student")
     List<Book> findAllWithStudents();
 
-    @Query("SELECT new com.examples.spring_jpa.examples.book.BookResponse(b.id, b.title, b.createdAt) " +
+    @Query("SELECT new com.examples.spring_jpa.examples.book.BookResponse(b.id, b.title) " +
             " FROM Book b")
     List<BookResponse> findAllBookResponse();
 
-    @Query("SELECT new com.examples.spring_jpa.examples.book.BookResponse(b.id, b.title, b.createdAt) " +
+    @Query("SELECT new com.examples.spring_jpa.examples.book.BookResponse(b.id, b.title) " +
             " FROM Book b " +
             " WHERE b.id = :id")
-    List<BookResponse> findBookResponseById(@Param("id") Long id);
+    Optional<BookResponse> findBookResponseById(@Param("id") Long id);
 }
